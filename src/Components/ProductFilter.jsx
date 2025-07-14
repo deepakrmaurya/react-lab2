3cfe458b7a6b00e4c0cd93faa7c6d9ab1a09f9cd
Deepak Maurya <deepakrmaurya@outlook.com>
2025-07-14 21:16:56 +0530

    set 2-10 task done

--- src/Components/ProductFilter.jsx
+++ src/Components/ProductFilter.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductRender from "./ProductRender";
+import products from "../constants/data";
+import NewProductCard from "./NewProductCard";
 
 const ProductFilter = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = () => {
+    console.log(searchText);
+  };
+
   return (
     <div>
       ProductFilter
-      <ProductRender />
+      <div>
+        <input
+          className="border-2 p-2 m-2 rounded-lg"
+          type="text"
+          placeholder="Search Here..."
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+          value={searchText}
+        />
+        <button onClick={handleSearch} className="border-2 p-2 m-2 rounded-lg">
+          Search
+        </button>
+      </div>
+      <div className="flex flex-wrap">
+        {products.map((data) => (
+          <NewProductCard
+            key={data.id}
+            name={data.name}
+            category={data.category}
+            price={data.price}
+            inStock={data.inStock}
+            rating={data.rating}
+            description={data.description}
+          />
+        ))}
+      </div>
     </div>
   );
 };
